8b836c8d0e8abc710c104fb44a0217e0350f36e8
sarka28 <118596293+sarka28@users.noreply.github.com>
2023-01-21 11:34:55 +0100

    Opravení signup

--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -10,9 +10,9 @@ LOGGER = getLogger()
 
 
 class SignUpView(CreateView):
-    template_name = 'accounts/signup.html'
+    template_name = 'sklad/signup.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('rooms')
+    success_url = reverse_lazy('signup')
 
     def form_valid(self, form):
         result = super().form_valid(form)
