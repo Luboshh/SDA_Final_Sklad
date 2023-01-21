92c1381bf61207089e1d7264fd5e3f494db1aa25
sarka28 <118596293+sarka28@users.noreply.github.com>
2023-01-21 09:56:19 +0100

    přidání registrace,login a password change

--- webapp/sklad/views.py
+++ webapp/sklad/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, FormView, CreateView, DeleteView
 
-from sklad.models import Item
+from webapp.sklad.models import Item
 
 # Create your views here.
 # Create your views here.
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
-from sklad import views
-from sklad import models
+from webapp.accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sklad/hello',views.hello),
+    path('sklad/hello', views.hello),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name="registration/password_change.html"),
+         name='password_change'),
+    path('accounts/password_change/done/',
+         PasswordChangeDoneView.as_view(template_name="registration/password_change_done.html"),
+         name='password_change_done'),
+    path('accounts/signup/', webapp.accounts.views.SignUpView.as_view(), name='signup'),
 ]
