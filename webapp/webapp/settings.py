371e9ac084b0062b10172b17f159184446f431dc
Luboshh <luboshhh@gmail.com>
2023-01-28 21:48:27 +0100

    Konečně to jede na railway 2

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -148,5 +148,5 @@ DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-ALLOWED_HOSTS = ['https://sdafinalsklad-production.up.railway.app/', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://sdafinalsklad-production.up.railway.app/']
+ALLOWED_HOSTS = ['sdafinalsklad-production.up.railway.app', '127.0.0.1', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['sdafinalsklad-production.up.railway.app']
